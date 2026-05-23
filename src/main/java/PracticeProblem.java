a60a3dfb681af59519bc9c11b23a9f0bdc64917a
LukasHerljevic <258844296+LukasHerljevic@users.noreply.github.com>
2026-05-23 13:48:14 +0000

    e

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,34 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    
+    public static boolean validIndex(int[] arr, int index) {
+        try {
+            
+            int value = arr[index];
+            return true;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            
+            return false;
+        }
+    }
 
-	}
+    
+    public static int divide(int num1, int num2) {
+        try {
+            return num1 / num2;
+        } catch (ArithmeticException e) {
+            
+            return 0;
+        }
+    }
 
-	
-
+    
+    public static int safeConvertStringtoInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            
+            return 0;
+        }
+    }
 }
